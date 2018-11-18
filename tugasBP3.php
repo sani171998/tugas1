34c864cf276a20f38e32404f0a696bcc07e66ec0
sani <43694342+sani171998@users.noreply.github.com>
2018-11-18 23:11:27 +0700

    Update tugasBP3.php

--- tugasBP3.php
+++ tugasBP3.php
@@ -7,9 +7,33 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
+  
+  .navbar {
+      margin-bottom: 0;
+      background-color: #f4511e;
+      z-index: 9999;
+      border: 0;
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+  .navbar li a, .navbar .navbar-brand {
+      color: #fff !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+      color: #f4511e !important;
+      background-color: #fff !important;
+  }
+  .navbar-default .navbar-toggle {
+      border-color: transparent;
+      color: #fff !important;
   </style>
 </head>
-<nav class="navbar navbar-inverse">
+<body id ="mypage" data-spy="scrool" data-target=".navbar"data-offset="70">
+
+<nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -27,16 +51,23 @@
             <li><a href="#">Pakaian Pria</a></li>
             <li><a href="#">Pakaian Wanita </a></li>
             <li><a href="#">Tas Pria</a></li>
+			<li><a href="#">Tas Wanita</a></li>
+			<li><a href="#">Celana Pria</a></li>
+			<li><a href="#">Celana Wanita</a></li>
+			<li><a href="#">sepatu Pria</a></li>
+			<li><a href="#">sepatu Wanita</a></li>
+			<li><a href="#">Topi</a></li>
 	</ul>
 	</li>
 	  <li class="navbar-form navbar-left" action="/action_page.php">
       <div class="form-group">
         <input type="text" class="form-control" placeholder="Cari Produk!" name="search">
       </div>
-      <button type="submit" class="btn btn-default">Cari</button>
+      <button type="submit" class="btn btn-center">Cari</button>
     </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+		 <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Masuk</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
       </ul>
     </div>
@@ -59,18 +90,18 @@
       <!-- Wrapper for slides -->
       <div class="carousel-inner" role="listbox">
         <div class="item active">
-          <img src="https://placehold.it/800x400?text=IMAGE" alt="Image">
+          <img src="assets/images/123.jpg" alt="Images">
           <div class="carousel-caption">
-            <h3>Sell $</h3>
-            <p>Money Money.</p>
+            <h3>Rp. 230.000</h3>
+            <p>Tas Ransel Free Raincover</p>
           </div>      
         </div>
 
         <div class="item">
-          <img src="https://placehold.it/800x400?text=Another Image Maybe" alt="Image">
+          <img src="assets/images/7.jpg" alt="Images">
           <div class="carousel-caption">
-            <h3>More Sell $</h3>
-            <p>Lorem ipsum...</p>
+            <h3>Rp. 100.000</h3>
+            <p>POLO SHIRT MUKO</p>
           </div>      
         </div>
       </div>
@@ -105,47 +136,42 @@
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-      </div>
+        <div class="panel-body"><img src="assets/images/5.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">Baju Kaos Lengan Panjang T-Shirt</div>
+        <div class="panel-footer">Rp. 60.000</div>
+	  </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-danger">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-body"><img src="assets/images/11.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-success">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-body"><img src="assets/images/123.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
       </div>
     </div>
   </div>
 </div><br>
-
 <div class="container">    
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
+
         <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
         <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
         <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
       </div>
